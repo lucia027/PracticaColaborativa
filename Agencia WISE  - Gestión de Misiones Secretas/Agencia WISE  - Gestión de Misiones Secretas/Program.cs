04c62fd470aa6a8561c9299ca7350e80f8d4bb1c
lucia027 <fuertes.lucia.27@gmail.com>
2025-11-22 12:02:16 +0100

    Creada función para listar misiones - Terminado

--- Agencia WISE  - Gestión de Misiones Secretas/Agencia WISE  - Gestión de Misiones Secretas/Program.cs
+++ Agencia WISE  - Gestión de Misiones Secretas/Agencia WISE  - Gestión de Misiones Secretas/Program.cs
@@ -108,7 +108,14 @@ void EliminarMision(Mision[]  misiones) {
 }
 
 void ListarMisiones(Mision[]  misiones) {
-    
+    WriteLine("--------------------------");
+    WriteLine("Id, Nombre, Riesgo, Agente");
+    WriteLine("--------------------------");
+
+    for (int i = 0; i < misiones.Length; i++) {
+        var mision = misiones[i];
+        WriteLine($"ID: {mision.Id}, Nombre: {mision.Nombre}, Riesgo{mision.NivelRiesgo},  Agente: {mision.AgenteAsignado}");
+    };
 }
 
 void ListaMisionesRiesgo(Mision[]  misiones) {
