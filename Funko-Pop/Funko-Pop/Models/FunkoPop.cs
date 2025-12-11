2d865f90acececbb611c0898f99ec4f445c684c2
lucia027 <fuertes.lucia.27@gmail.com>
2025-12-11 13:33:37 +0100

    Añadido del model -Terminado

--- Funko-Pop/Funko-Pop/Models/FunkoPop.cs
+++ Funko-Pop/Funko-Pop/Models/FunkoPop.cs
@@ -1,5 +1,13 @@
 ﻿namespace Funko_Pop.Models;
 
 public class FunkoPop {
+    public int Id { get; set; } = 0;
+    public required string Nombre { get; set; }
+    public required decimal Precio { get; set; }
+    public required Tipo Categoria { get; set; }
     
+
+    public enum Tipo {
+        Superheroe, Anime, Disney
+    }
 }
