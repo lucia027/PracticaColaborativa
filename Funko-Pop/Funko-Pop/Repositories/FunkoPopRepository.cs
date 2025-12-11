1a203a7bbfb4980cdd667da305f852722e5b2f37
lucia027 <fuertes.lucia.27@gmail.com>
2025-12-11 13:34:06 +0100

    Añadido del respositorio -Sin terminar

--- Funko-Pop/Funko-Pop/Repositories/FunkoPopRepository.cs
+++ Funko-Pop/Funko-Pop/Repositories/FunkoPopRepository.cs
@@ -1,5 +1,29 @@
-﻿namespace Funko_Pop.Repository;
+﻿using Funko_Pop.Factories;
+using Funko_Pop.Models;
+using Serilog;
 
+namespace Funko_Pop.Repositories;
+/// <summary>
+///  Clase que gestiona 
+/// </summary>
 public class FunkoPopRepository {
     
+    private readonly ILogger _log = Log.ForContext<FunkoPopRepository>();
+    private int _contadorId = 5;
+    private FunkoPop[] _funkos = FunkoPopFactory.DatosBase();
+    
+    private int NewId() {
+        Log.Debug("Generando nuevo id..");
+        return _contadorId++;
+    }
+
+    private void Aumentar() {
+    }
+    
+    private void Disminuir() {
+    }
+
+    private FunkoPop Save(FunkoPop funko) {
+        
+    }
 }
