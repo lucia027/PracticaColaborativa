dbc9e29b7d4d9341e6bf2e89e08fd45d135b9628
lucia027 <fuertes.lucia.27@gmail.com>
2025-12-15 19:32:56 +0100

    Definida la DAW's template y creada la funcion de mostra menu, validar opcion del menu y empexada la de elegirla. - Sin terminar

--- Funko-Pop/Funko-Pop/Program.cs
+++ Funko-Pop/Funko-Pop/Program.cs
@@ -1,3 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using Funko_Pop.Repositories;
+using Funko_Pop.Service;
+using Funko_Pop.Validator;
+using Serilog;
+using static System.Console;
 
-Console.WriteLine("Hello, World!");
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug() // Permitir mensajes Debug y superiores
+    .WriteTo.Console() // Salida a consola
+    .CreateLogger(); // Utilizamos Serilog para el logging
+
+
+// Configuración de consola y Encoding
+Title = "";
+OutputEncoding = Encoding.UTF8;
+Clear();
+
+// Constantes y variables globales (Justificadas)
+
+
+// Programa principal
+Main();
+
+// Limpieza de logs y salida
+Log.CloseAndFlush(); // Asegura que todos los logs pendientes se escriban.
+WriteLine("\n⌨️ Presiona una tecla para salir...");
+ReadKey();
+
+// Programa principal
+void Main() {
+    var service = new FunkoPopService(FunkoPopRepository.GetInstance(), new FunkoPopValidator());
+    
+    WriteLine("-------------------------------------------------------");
+    WriteLine("   ¡Bienvenido al programa de gestion de FunkoPops!    ");
+    WriteLine("-------------------------------------------------------");
+
+    MostrarMenu();
+    
+}
+
+// Métodos auxiliares
+void MostrarMenu() {
+    WriteLine("------------------------");
+    WriteLine("          Menú:         ");
+    WriteLine("------------------------");
+    WriteLine("1·  Listar FunkoPops.");
+    WriteLine("2·  Buscar FunkoPop.");
+    WriteLine("3·  Crear FunkoPop.");
+    WriteLine("4·  Actualizar FunkoPop.");
+    WriteLine("5·  Eliminar FunkoPop.");
+    WriteLine("");
+    OpcionMenu();
+}
+
+void OpcionMenu() {
+    bool isValida;
+
+    do {
+        WriteLine("Elige una opcion valida del menú:");
+        var opcion = ReadLine() ?? "0".Trim();
+        isValida = ValidarOpcionMenu(opcion);
+    } while (!isValida);
+}
+
+bool ValidarOpcionMenu(string opcion) {
+    if (opcion != "1" && opcion != "2" && opcion != "3" && opcion != "4" && opcion != "5") {
+        return false;
+    }
+    return true;
+}
