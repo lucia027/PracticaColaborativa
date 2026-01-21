c83a8d35713ea04d58284e6d2ee82370966371a5
lucia027 <fuertes.lucia.27@gmail.com>
2026-01-21 10:57:53 +0100

    Modelos finalizados

---
+++ Genericos/Genericos/Models/CajaEnteros.cs
@@ -0,0 +1,9 @@
+﻿namespace Genericos.Models;
+
+public class CajaEnteros(int valor) {
+    private int _valor = valor;
+    public int GetValor => _valor;
+    public void SetValor(int valorNuevo) {
+        _valor = valorNuevo;
+    }
+}
---
+++ Genericos/Genericos/Models/CajaString.cs
@@ -0,0 +1,9 @@
+﻿namespace Genericos.Models;
+
+public class CajaString(string valor) {
+    private string _valor = valor;
+    public string GetValor => _valor;
+    public void SetValor(string valorNuevo) {
+        _valor = valorNuevo;
+    }
+}
