1369e3a904b097f2e85749e6b021ca03e84dae39
lucia027 <fuertes.lucia.27@gmail.com>
2026-02-27 12:40:40 +0100

    Validadores empezados

--- Laboratorio Digital del Palacio Interior/Laboratorio Digital del Palacio Interior/Validator/CasoMedicoValidator.cs
+++ Laboratorio Digital del Palacio Interior/Laboratorio Digital del Palacio Interior/Validator/CasoMedicoValidator.cs
@@ -1,5 +1,35 @@
-﻿namespace Laboratorio_Digital_del_Palacio_Interior.Validator;
+﻿using Laboratorio_Digital_del_Palacio_Interior.Enums;
+using Laboratorio_Digital_del_Palacio_Interior.Models;
+using Laboratorio_Digital_del_Palacio_Interior.Validator.Common;
 
-public class CasoMedicoValidator {
-     
+namespace Laboratorio_Digital_del_Palacio_Interior.Validator;
+
+public class CasoMedicoValidator : IValidator<CasoMedico> {
+    public IEnumerable<string> Validar(CasoMedico entity ) {
+        var errores = new List<string>();
+
+        if (string.IsNullOrEmpty(entity.Sintomas)) {
+            errores.Add("ERROR - Los sintomas no pueden ser nulo o estar vacio.");
+        }
+
+        if (!Enum.IsDefined(typeof(Gravedad), entity.Gravedad)) {
+            errores.Add("ERROR - La gravedad solo puede ser nulo, leve, moderado o grave.");
+        }
+
+        if (!Enum.IsDefined(typeof(CausaSospecha), entity.CausaSospecha)) {
+            errores.Add("ERROR - La gravedad solo puede ser enfermedad, veneno, desconocida.");
+        }
+        
+        if(sustancias sospechosas){
+        }
+
+        if(tratamientos aplicados){
+        }
+
+        if (!Enum.IsDefined(typeof(EstadoCasoMedico), entity.Estado)) {
+            errores.Add("ERROR - El estado del caso medico solo puede ser abierto, resuelto o en investigacion.");
+        }
+
+        return errores;
+    }
 }
