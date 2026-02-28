e242e6a8b441f92f584b945200cf78308cbd57a3
lucia027 <fuertes.lucia.27@gmail.com>
2026-02-28 11:22:52 +0100

    Validadores termiandos

--- Laboratorio Digital del Palacio Interior/Laboratorio Digital del Palacio Interior/Validator/CasoMedicoValidator.cs
+++ Laboratorio Digital del Palacio Interior/Laboratorio Digital del Palacio Interior/Validator/CasoMedicoValidator.cs
@@ -20,10 +20,12 @@ public class CasoMedicoValidator : IValidator<CasoMedico> {
             errores.Add("ERROR - La gravedad solo puede ser enfermedad, veneno, desconocida.");
         }
         
-        if(sustancias sospechosas){
+        if(!entity.SustanciasSospechosas.Any()){
+            errores.Add("ERROR - La colección de sustancias sospechosas esta vacia.");
         }
 
-        if(tratamientos aplicados){
+        if(!entity.Tratamientos.Any()){
+            errores.Add("ERROR - La colección de tratamientos esta vacia.");
         }
 
         if (!Enum.IsDefined(typeof(EstadoCasoMedico), entity.Estado)) {
