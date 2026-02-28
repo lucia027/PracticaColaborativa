023f3c5e68d375f5971fe9165f2af9948f3087a3
lucia027 <fuertes.lucia.27@gmail.com>
2026-02-28 13:39:59 +0100

    Factory termiandos

---
+++ Laboratorio Digital del Palacio Interior/Laboratorio Digital del Palacio Interior/Factory/CasosMedicosFactory.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Laboratorio_Digital_del_Palacio_Interior.Enums;
+using Laboratorio_Digital_del_Palacio_Interior.Models;
+
+namespace Laboratorio_Digital_del_Palacio_Interior.Factory;
+
+public class CasosMedicosFactory {
+    public static List<CasoMedico> Seed() {
+        var datosBase = new List<CasoMedico>();
+
+        var sustancias = SustanciaFactory.Seed();
+        var medicinas = sustancias.OfType<Medicina>().ToList();
+        var venenos = sustancias.OfType<Veneno>().ToList();
+        
+        datosBase.Add(new CasoMedico(
+            sintomas: "Palidez extrema, debilidad general, dolores de cabeza y náuseas recurrentes en la concubina del Pabellón de Jade.",
+            gravedad: Gravedad.Grave, 
+            causaSospecha: CausaSospecha.Veneno, 
+            sustanciasSospechosas: new HashSet<Veneno?> { 
+                venenos.First(v => v.Nombre == "Arsenico Blanco Refinado") 
+            },
+            tratamientos: new HashSet<Medicina> { 
+                medicinas.First(m => m.Nombre == "Antidoto Imperial contra Arsénico"),
+                medicinas.First(m => m.Nombre == "Tonico Revitalizante de Palacio")
+            },
+            estado: EstadoCasoMedico.Resuelto
+        ));
+
+        datosBase.Add(new CasoMedico(
+            sintomas: "Dolor abdominal agudo y mareos repentinos tras consumir la sopa fría servida por los eunucos.",
+            gravedad: Gravedad.Moderada,
+            causaSospecha: CausaSospecha.Enfermedad,
+            sustanciasSospechosas: new HashSet<Veneno?> { 
+                venenos.First(v => v.Nombre == "Polvo Gastrotoxico Irritante") 
+            },
+            tratamientos: new HashSet<Medicina> { 
+                medicinas.First(m => m.Nombre == "Polvo Digestivo de Hierbas"),
+                medicinas.First(m => m.Nombre == "Infusion Detoxificante de Maomao")
+            },
+            estado: EstadoCasoMedico.Abierto
+        ));
+
+        datosBase.Add(new CasoMedico(
+            sintomas: "Convulsiones repentinas, alucinaciones y pérdida de conocimiento.",
+            gravedad: Gravedad.Grave,
+            causaSospecha: CausaSospecha.Veneno,
+            sustanciasSospechosas: new HashSet<Veneno?> { 
+                venenos.First(v => v.Nombre == "Extracto Neurotoxico de Hongo Negro"),
+                venenos.First(v => v.Nombre == "Toxina Paralizante de Serpiente")
+            },
+            tratamientos: new HashSet<Medicina> { 
+                medicinas.First(m => m.Nombre == "Elixir Anticonvulsivo del Pabellon Jade")
+            },
+            estado: EstadoCasoMedico.EnInvestigacion
+        ));
+
+        datosBase.Add(new CasoMedico(
+            sintomas: "Tos seca persistente que empeora al encender los braseros aromáticos en la habitación.",
+            gravedad: Gravedad.Leve,
+            causaSospecha: CausaSospecha.Desconocida,
+            sustanciasSospechosas: new HashSet<Veneno?> { 
+                venenos.First(v => v.Nombre == "Polvo Toxico de Concubina") 
+            },
+            tratamientos: new HashSet<Medicina> { 
+                medicinas.First(m => m.Nombre == "Jarabe Respiratorio Imperial")
+            },
+            estado: EstadoCasoMedico.Resuelto
+        ));
+
+        return datosBase;
+    }
+}
---
+++ Laboratorio Digital del Palacio Interior/Laboratorio Digital del Palacio Interior/Factory/SustanciaFactory.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using Laboratorio_Digital_del_Palacio_Interior.Enums;
+using Laboratorio_Digital_del_Palacio_Interior.Models;
+
+namespace Laboratorio_Digital_del_Palacio_Interior.Factory;
+
+public static class SustanciaFactory {
+    public static List<Sustancia> Seed() {
+        var datosBase = new List<Sustancia>();
+
+        datosBase.Add(new Medicina(
+            nombre: "Antidoto Imperial contra Arsénico",
+            descripcion: "Neutraliza intoxicaciones por arsenico",
+            precio: 45,
+            disponibilidad: Disponibilidad.Rara,
+            nivelPeligro: Peligro.Bajo,
+            sintoma: "Intoxicacion por arsenico",
+            dosisRecomedada: 1.5,
+            efectosSecundario: "Nauseas leves",
+            tiempoEfecto: 30
+            ));
+            
+        datosBase.Add(new Medicina(
+            nombre: "Decoccion Antipirético Imperial",
+            descripcion: "Reduce la fiebre alta",
+            precio: 60,
+            disponibilidad: Disponibilidad.Comun,
+            nivelPeligro: Peligro.Bajo,
+            sintoma: "Fiebre alta",
+            dosisRecomedada: 1.5,
+            efectosSecundario: "Somnolencia",
+            tiempoEfecto: 45
+        ));
+
+        datosBase.Add(new Medicina(
+            nombre: "Polvo Digestivo de Hierbas",
+            descripcion: "Alivia dolores estomacales",
+            precio: 25,
+            disponibilidad: Disponibilidad.MuyRara,
+            nivelPeligro: Peligro.Bajo,
+            sintoma: "Dolor abdominal",
+            dosisRecomedada: 1.0,
+            efectosSecundario: "Gases",
+            tiempoEfecto: 20
+        ));
+
+        datosBase.Add(new Medicina(
+            nombre: "Elixir Anticonvulsivo del Pabellon Jade",
+            descripcion: "Reduce convulsiones",
+            precio: 80,
+            disponibilidad: Disponibilidad.Comun,
+            nivelPeligro: Peligro.Medio,
+            sintoma: "Convulsiones",
+            dosisRecomedada: 0.8,
+            efectosSecundario: "Mareo",
+            tiempoEfecto: 25
+        ));
+
+        datosBase.Add(new Medicina(
+            nombre: "Infusion Detoxificante de Maomao",
+            descripcion: "Neutraliza toxinas leves",
+            precio: 35,
+            disponibilidad: Disponibilidad.Rara,
+            nivelPeligro: Peligro.Bajo,
+            sintoma: "Envenenamiento leve",
+            dosisRecomedada: 2.0,
+            efectosSecundario: "Sudoracion",
+            tiempoEfecto: 40
+        ));
+
+        datosBase.Add(new Medicina(
+            nombre: "Jarabe Respiratorio Imperial",
+            descripcion: "Tratamiento para la tos",
+            precio: 55,
+            disponibilidad: Disponibilidad.Rara,
+            nivelPeligro: Peligro.Bajo,
+            sintoma: "Tos",
+            dosisRecomedada: 1.2,
+            efectosSecundario: "Sequedad bucal",
+            tiempoEfecto: 35
+        ));
+
+        datosBase.Add(new Medicina(
+            nombre: "Tonico Revitalizante de Palacio",
+            descripcion: "Recupera energia tras intoxicaciones",
+            precio: 70,
+            disponibilidad: Disponibilidad.Comun,
+            nivelPeligro: Peligro.Bajo,
+            sintoma: "Debilidad",
+            dosisRecomedada: 1.0,
+            efectosSecundario: "Insomnio leve",
+            tiempoEfecto: 60
+        ));
+
+        return datosBase;
+    }
+}
