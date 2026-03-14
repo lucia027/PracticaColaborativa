8d99ffdc565ebc218ea29a1f0e00b36ff49343ab
lucia027 <fuertes.lucia.27@gmail.com>
2026-03-14 20:52:00 +0100

    Añadido de datos

--- Laboratorio Digital del Palacio Interior/Laboratorio Digital del Palacio Interior/Factory/SustanciaFactory.cs
+++ Laboratorio Digital del Palacio Interior/Laboratorio Digital del Palacio Interior/Factory/SustanciaFactory.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Laboratorio_Digital_del_Palacio_Interior.Enums;
+﻿using Laboratorio_Digital_del_Palacio_Interior.Enums;
 using Laboratorio_Digital_del_Palacio_Interior.Models;
 
 namespace Laboratorio_Digital_del_Palacio_Interior.Factory;
@@ -18,33 +17,33 @@ public static class SustanciaFactory {
             DosisRecomendada = 1.5,
             EfectosSecundarios = "Nauseas leves",
             TiempoEfecto = 30
-            });
-            
-        datosBase.Add(new Medicina{
+        });
+
+        datosBase.Add(new Medicina {
             Nombre = "Decoccion Antipirético Imperial",
             Descripcion = "Reduce la fiebre alta",
             Precio = 60,
             Disponibilidad = Disponibilidad.Comun,
             NivelPeligro = Peligro.Bajo,
             Sintoma = "Fiebre alta",
-            DosisRecomendada =  1.5,
-            EfectosSecundarios ="Somnolencia",
+            DosisRecomendada = 1.5,
+            EfectosSecundarios = "Somnolencia",
             TiempoEfecto = 45
         });
 
-        datosBase.Add(new Medicina { 
+        datosBase.Add(new Medicina {
             Nombre = "Polvo Digestivo de Hierbas",
             Descripcion = "Alivia dolores estomacales",
             Precio = 25,
             Disponibilidad = Disponibilidad.MuyRara,
             NivelPeligro = Peligro.Bajo,
-            Sintoma =  "Dolor abdominal",
+            Sintoma = "Dolor abdominal",
             DosisRecomendada = 1.0,
-            EfectosSecundarios ="Gases",
+            EfectosSecundarios = "Gases",
             TiempoEfecto = 20
         });
 
-        datosBase.Add(new Medicina{
+        datosBase.Add(new Medicina {
             Nombre = "Elixir Anticonvulsivo del Pabellon Jade",
             Descripcion = "Reduce convulsiones",
             Precio = 80,
@@ -56,7 +55,7 @@ public static class SustanciaFactory {
             TiempoEfecto = 25
         });
 
-        datosBase.Add(new Medicina{
+        datosBase.Add(new Medicina {
             Nombre = "Infusion Detoxificante de Maomao",
             Descripcion = "Neutraliza toxinas leves",
             Precio = 35,
@@ -68,11 +67,11 @@ public static class SustanciaFactory {
             TiempoEfecto = 40
         });
 
-        datosBase.Add(new Medicina{
+        datosBase.Add(new Medicina {
             Nombre = "Jarabe Respiratorio Imperial",
             Descripcion = "Tratamiento para la tos",
             Precio = 55,
-            Disponibilidad =  Disponibilidad.Rara,
+            Disponibilidad = Disponibilidad.Rara,
             NivelPeligro = Peligro.Bajo,
             Sintoma = "Tos",
             DosisRecomendada = 1.2,
@@ -80,16 +79,184 @@ public static class SustanciaFactory {
             TiempoEfecto = 35
         });
 
-        datosBase.Add(new Medicina{
+        datosBase.Add(new Medicina {
             Nombre = "Tonico Revitalizante de Palacio",
             Descripcion = "Recupera energia tras intoxicaciones",
             Precio = 70,
-            Disponibilidad =  Disponibilidad.Comun,
-            NivelPeligro =  Peligro.Bajo,
+            Disponibilidad = Disponibilidad.Comun,
+            NivelPeligro = Peligro.Bajo,
             Sintoma = "Debilidad",
             DosisRecomendada = 1.0,
             EfectosSecundarios = "Insomnio leve",
-            TiempoEfecto =  60
+            TiempoEfecto = 60
+        });
+
+        datosBase.Add(new Veneno {
+            Id = 1,
+            Nombre = "Polvo de Nieve Fría",
+            Descripcion = "Un veneno silencioso usado en la corte. Produce escalofríos y debilidad progresiva.",
+            Precio = 120,
+            Disponibilidad = Disponibilidad.Rara,
+            NivelPeligro = Peligro.Alto,
+            ViaDeAdministracion = ViaDeAdministracion.Contacto,
+            TiempoAparicion = 45,
+            Antidoto = datosBase
+                .OfType<Medicina>()
+                .First(m => m.Nombre == "Antidoto Imperial contra Arsénico"),
+            GradoToxicidad = 8
+        });
+
+        datosBase.Add(new Veneno {
+            Id = 2,
+            Nombre = "Lágrimas de la Flor Fantasma",
+            Descripcion = "Extracto de una flor venenosa que causa desorientación y pérdida de fuerza.",
+            Precio = 95,
+            Disponibilidad = Disponibilidad.Comun,
+            NivelPeligro = Peligro.Medio,
+            ViaDeAdministracion = ViaDeAdministracion.Oral,
+            TiempoAparicion = 20,
+            Antidoto = datosBase
+                .OfType<Medicina>()
+                .First(m => m.Nombre == "Decoccion Antipirético Imperial"),
+            GradoToxicidad = 6
+        });
+
+        datosBase.Add(new Veneno {
+            Id = 3,
+            Nombre = "Aguja de Serpiente Carmesí",
+            Descripcion = "Veneno derivado de un reptil exótico. Provoca entumecimiento y fiebre.",
+            Precio = 180,
+            Disponibilidad = Disponibilidad.MuyRara,
+            NivelPeligro = Peligro.Alto,
+            ViaDeAdministracion = ViaDeAdministracion.Contacto,
+            TiempoAparicion = 10,
+            Antidoto = datosBase
+                .OfType<Medicina>()
+                .First(m => m.Nombre == "Polvo Digestivo de Hierbas"),
+            GradoToxicidad = 9
+        });
+
+        datosBase.Add(new Veneno {
+            Id = 4,
+            Nombre = "Polvo de Jade Marchito",
+            Descripcion = "Sustancia usada en intrigas palaciegas. Causa debilidad muscular y mareos.",
+            Precio = 70,
+            Disponibilidad = Disponibilidad.Comun,
+            NivelPeligro = Peligro.Medio,
+            ViaDeAdministracion = ViaDeAdministracion.Inhalacion,
+            TiempoAparicion = 30,
+            Antidoto = datosBase
+                .OfType<Medicina>()
+                .First(m => m.Nombre == "Elixir Anticonvulsivo del Pabellon Jade"),
+            GradoToxicidad = 5
+        });
+
+        datosBase.Add(new Veneno {
+            Id = 5,
+            Nombre = "Néctar de la Noche",
+            Descripcion = "Un veneno líquido oscuro que provoca somnolencia profunda.",
+            Precio = 150,
+            Disponibilidad = Disponibilidad.Rara,
+            NivelPeligro = Peligro.Alto,
+            ViaDeAdministracion = ViaDeAdministracion.Oral,
+            TiempoAparicion = 15,
+            Antidoto = datosBase
+                .OfType<Medicina>()
+                .First(m => m.Nombre == "Infusion Detoxificante de Maomao"),
+            GradoToxicidad = 7
+        });
+
+        datosBase.Add(new Veneno {
+            Id = 6,
+            Nombre = "Humo de la Luna Roja",
+            Descripcion = "Sustancia quemada para liberar vapores que causan confusión.",
+            Precio = 110,
+            Disponibilidad = Disponibilidad.Rara,
+            NivelPeligro = Peligro.Medio,
+            ViaDeAdministracion = ViaDeAdministracion.Inhalacion,
+            TiempoAparicion = 5,
+            Antidoto = datosBase
+                .OfType<Medicina>()
+                .First(m => m.Nombre == "Jarabe Respiratorio Imperial"),
+            GradoToxicidad = 6
+        });
+
+        datosBase.Add(new Afrodisiaco {
+            Id = 7,
+            Nombre = "Perfume de la Flor Carmesí",
+            Descripcion = "Aroma dulce que estimula el ánimo y la cercanía emocional.",
+            Precio = 60,
+            Disponibilidad = Disponibilidad.Comun,
+            NivelPeligro = Peligro.Bajo,
+            IntensidadEfecto = 4,
+            Duracion = 120,
+            ContraIndicaciones = "Evitar en personas con alergias fuertes.",
+            RiegosUso = "Ligero enrojecimiento facial."
+        });
+
+        datosBase.Add(new Afrodisiaco {
+            Id = 8,
+            Nombre = "Elixir de la Luna Plateada",
+            Descripcion = "Bebida suave que aumenta la calidez corporal y la sensibilidad.",
+            Precio = 85,
+            Disponibilidad = Disponibilidad.Rara,
+            NivelPeligro = Peligro.Bajo,
+            IntensidadEfecto = 6,
+            Duracion = 90,
+            ContraIndicaciones = "No mezclar con alcohol.",
+            RiegosUso = "Somnolencia leve."
+        });
+
+        datosBase.Add(new Afrodisiaco {
+            Id = 9,
+            Nombre = "Polvo de Flor de Durazno",
+            Descripcion = "Sustancia aromática usada en rituales románticos.",
+            Precio = 40,
+            Disponibilidad = Disponibilidad.Comun,
+            NivelPeligro = Peligro.Bajo,
+            IntensidadEfecto = 3,
+            Duracion = 60,
+            ContraIndicaciones = "Evitar en embarazadas.",
+            RiegosUso = "Picor nasal ocasional."
+        });
+
+        datosBase.Add(new Afrodisiaco {
+            Id = 10,
+            Nombre = "Té de Pétalos Rosados",
+            Descripcion = "Infusión suave que relaja y mejora el estado de ánimo.",
+            Precio = 30,
+            Disponibilidad = Disponibilidad.Comun,
+            NivelPeligro = Peligro.Bajo,
+            IntensidadEfecto = 2,
+            Duracion = 45,
+            ContraIndicaciones = "No recomendado para hipertensos.",
+            RiegosUso = "Ligera aceleración del pulso."
+        });
+
+        datosBase.Add(new Afrodisiaco {
+            Id = 11,
+            Nombre = "Esencia de Orquídea Dorada",
+            Descripcion = "Extracto aromático muy apreciado en la nobleza.",
+            Precio = 120,
+            Disponibilidad = Disponibilidad.MuyRara,
+            NivelPeligro = Peligro.Medio,
+            IntensidadEfecto = 7,
+            Duracion = 150,
+            ContraIndicaciones = "Evitar en personas con problemas cardíacos.",
+            RiegosUso = "Aumento temporal de la temperatura corporal."
+        });
+
+        datosBase.Add(new Afrodisiaco {
+            Id = 12,
+            Nombre = "Néctar de Melocotón Dulce",
+            Descripcion = "Jarabe suave que genera sensación de bienestar.",
+            Precio = 55,
+            Disponibilidad = Disponibilidad.Comun,
+            NivelPeligro = Peligro.Bajo,
+            IntensidadEfecto = 5,
+            Duracion = 80,
+            ContraIndicaciones = "No apto para diabéticos.",
+            RiegosUso = "Ligera somnolencia."
         });
 
         return datosBase;
