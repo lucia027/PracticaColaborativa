d19af043617977110bf68d5feb67fcbc4f28d977
lucia027 <fuertes.lucia.27@gmail.com>
2026-03-15 13:38:27 +0100

    Dtos terminados

---
+++ Laboratorio Digital del Palacio Interior/Laboratorio Digital del Palacio Interior/Dto/CasosMedicosDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Laboratorio_Digital_del_Palacio_Interior.Dto;
+
+public record CasosMedicosDto(
+    int Id,
+    string Sintomas,
+    string FechaInicio,
+    string Gravedad,
+    string CausaSospecha,
+    string? SustanciasSospechosas,
+    string? Tratamientos,
+    string Estado
+) { }
