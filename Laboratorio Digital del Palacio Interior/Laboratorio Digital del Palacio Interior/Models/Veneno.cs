ce8c94e0997dcf3cbe6f1ea8fed9b3244006e957
lucia027 <fuertes.lucia.27@gmail.com>
2026-03-20 15:09:00 +0100

    Cambio del tipo de un atributo.

--- Laboratorio Digital del Palacio Interior/Laboratorio Digital del Palacio Interior/Models/Veneno.cs
+++ Laboratorio Digital del Palacio Interior/Laboratorio Digital del Palacio Interior/Models/Veneno.cs
@@ -8,7 +8,7 @@ namespace Laboratorio_Digital_del_Palacio_Interior.Models;
 public sealed record Veneno : Sustancia {
     public ViaDeAdministracion ViaDeAdministracion { get; init; }
     public int TiempoAparicion { get; init; }
-    public Medicina Antidoto { get; init; }
+    public Medicina? Antidoto { get; init; }
     public int GradoToxicidad { get; init; }
     public int ProbabilidadSupervivencia => (GradoToxicidad >= 8) ? 20 : 60;
     
