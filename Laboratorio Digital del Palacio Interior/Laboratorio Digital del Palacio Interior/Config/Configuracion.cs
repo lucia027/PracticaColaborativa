f5e53de9752e367a46b864ef6541459632571a3a
lucia027 <fuertes.lucia.27@gmail.com>
2026-03-20 14:38:22 +0100

    Añadido de files para el Json.

--- Laboratorio Digital del Palacio Interior/Laboratorio Digital del Palacio Interior/Config/Configuracion.cs
+++ Laboratorio Digital del Palacio Interior/Laboratorio Digital del Palacio Interior/Config/Configuracion.cs
@@ -1,5 +1,8 @@
 ﻿namespace Laboratorio_Digital_del_Palacio_Interior.Config;
 
 public static class Configuracion {
-    public static string DataFolder => Path.Combine(Environment.CurrentDirectory, "data");
+    public static readonly string DataFolder = Path.Combine(Environment.CurrentDirectory, "data");
+    public static readonly string SustanciaFile = Path.Combine(DataFolder, "sustanciaFile.json");
+    public static readonly string CasosMedicosFile = Path.Combine(DataFolder, "casosMedicosFile.json");
+
 }
