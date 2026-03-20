5231c0116302615dd18f1d971043001d485f3c26
lucia027 <fuertes.lucia.27@gmail.com>
2026-03-20 17:27:33 +0100

    Avance

--- Laboratorio Digital del Palacio Interior/Laboratorio Digital del Palacio Interior/obj/Debug/net10.0/Laboratorio Digital del Palacio Interior.AssemblyInfo.cs
+++ Laboratorio Digital del Palacio Interior/Laboratorio Digital del Palacio Interior/obj/Debug/net10.0/Laboratorio Digital del Palacio Interior.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio Digital del Palacio Interior")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e53de9752e367a46b864ef6541459632571a3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+125b0b5cb36a4b36d2a8f9bc0b6ead41bf8a066a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio Digital del Palacio Interior")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio Digital del Palacio Interior")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
